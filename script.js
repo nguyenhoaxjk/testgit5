69e583f451e506d847f2d319d947a8316bb2406d
nguyenhoaxjk <134728644+nguyenhoaxjk@users.noreply.github.com>
2026-09-13 13:59:43 +0700

    test_2

---
+++ script.js
@@ -0,0 +1,18 @@
+const menhGia = {
+    VND : 1,
+    // 1 : 26.179 USD
+    USD : 0.0000382
+ 
+};
+function doiTien(){
+    let tien = document.getElementById("nhapTien").value;
+    let tu = document.getElementById("tu").value;
+    let sang = document.getElementById("sang").value;
+    let ketQua = tien / (menhGia[tu] * menhGia[sang]);
+    let ketQuaXuat = document.getElementById("xuatTien");
+    ketQuaXuat.textContent = `${ ketQua }`;
+}
+
+
+
+
